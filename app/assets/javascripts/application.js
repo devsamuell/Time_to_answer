14f2181a764c3ee9277003531df268614f79ed8a
Jackson Pires <jack@jack.com>
2023-03-05 03:04:11 +0000

    Jq

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery.ui.all
 //= require rails-ujs
 //= require activestorage
